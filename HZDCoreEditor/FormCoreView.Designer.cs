6217ed364aa17103df214e23e24bac46f7528e2a
Nukem <Nukem@outlook.com>
2020-09-16 05:34:57 -0400

    Add basic UI for viewing core objects

---
+++ HZDCoreEditor/FormCoreView.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace HZDCoreEditor
+{
+    partial class FormCoreView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Size = new System.Drawing.Size(1570, 877);
+            this.splitContainer.SplitterDistance = 523;
+            this.splitContainer.TabIndex = 0;
+            this.splitContainer.Text = "splitContainer1";
+            // 
+            // FormCoreView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1570, 877);
+            this.Controls.Add(this.splitContainer);
+            this.Name = "FormCoreView";
+            this.Text = "FormCoreView";
+            this.Load += new System.EventHandler(this.FormCoreView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+    }
+}
