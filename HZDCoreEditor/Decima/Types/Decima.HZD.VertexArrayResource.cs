0a662621e9817dc537d67d6a68602d769bd12f63
Nukem <Nukem@outlook.com>
2020-09-25 02:52:43 -0400

    Decode unknown bytes in VertexArrayResource

--- HZDCoreEditor/Decima/Types/Decima.HZD.VertexArrayResource.cs
+++ HZDCoreEditor/Decima/Types/Decima.HZD.VertexArrayResource.cs
@@ -1,65 +1,87 @@
 using Utility;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Decima.HZD
 {
     [RTTI.Serializable(0xBBAB0E0254767A94)]
     public class VertexArrayResource : BaseResource, RTTI.IExtraBinaryDataCallback
     {
-        public uint VertexElementCount; // TODO: Determine from HwBuffer entries (what happens if there's none?)
-        public VertexStream[] Streams;
-        public bool IsStreaming;        // TODO: Determine from HwBuffer entries (what happens if there's none?)
+        public uint VertexCount;    // TODO: Determine from HwBuffer entries (what happens if there's none?)
+        public List<VertexStream> Streams;
+        public bool IsStreaming;    // TODO: Determine from HwBuffer entries (what happens if there's none?)
 
         public class VertexStream
         {
             public uint Flags;
-            public byte[][] UnknownData;
+            public List<VertexElementDesc> ElementInfo;
             public GGUUID GUID;
             public HwBuffer Buffer;
         }
 
+        public class VertexElementDesc
+        {
+            public byte ByteOffset;                         // Ex: 0
+            public EVertexElementStorageType StorageType;   // Ex: SignedShortNormalized
+            public byte ComponentCount;                     // Ex: 3 (Pos -> X, Y, Z)
+            public EVertexElement ElementType;              // Ex: Pos
+        }
+
         public void DeserializeExtraData(BinaryReader reader)
         {
-            VertexElementCount = reader.ReadUInt32();
-            uint vertexStreamCount = reader.ReadUInt32();
+            VertexCount = reader.ReadUInt32();
+            uint streamCount = reader.ReadUInt32();
             IsStreaming = reader.ReadBooleanStrict();
 
-            Streams = new VertexStream[vertexStreamCount];
+            Streams = new List<VertexStream>((int)streamCount);
 
-            for (int i = 0; i < Streams.Length; i++)
+            for (uint i = 0; i < streamCount; i++)
             {
+                uint flags = reader.ReadUInt32();
+                uint byteStride = reader.ReadUInt32();
+                uint elementDescCount = reader.ReadUInt32();
+
                 var stream = new VertexStream();
-                Streams[i] = stream;
+                stream.Flags = flags;
+                stream.ElementInfo = new List<VertexElementDesc>((int)elementDescCount);
 
-                stream.Flags = reader.ReadUInt32();
-                uint vertexByteStride = reader.ReadUInt32();
-                stream.UnknownData = new byte[reader.ReadUInt32()][];
-
-                for (uint j = 0; j < stream.UnknownData.Length; j++)
+                for (uint j = 0; j < elementDescCount; j++)
                 {
-                    // 4 bytes read separately
-                    stream.UnknownData[j] = reader.ReadBytesStrict(4);
+                    stream.ElementInfo.Add(new VertexElementDesc()
+                    {
+                        ByteOffset = reader.ReadByte(),
+                        StorageType = (EVertexElementStorageType)reader.ReadByte(),
+                        ComponentCount = reader.ReadByte(),
+                        ElementType = (EVertexElement)reader.ReadByte(),
+                    });
                 }
 
                 stream.GUID = GGUUID.FromData(reader);
-                stream.Buffer = HwBuffer.FromVertexData(reader, IsStreaming, vertexByteStride, VertexElementCount);
+                stream.Buffer = HwBuffer.FromVertexData(reader, IsStreaming, byteStride, VertexCount);
+
+                Streams.Add(stream);
             }
         }
 
         public void SerializeExtraData(BinaryWriter writer)
         {
-            writer.Write(VertexElementCount);
-            writer.Write((uint)Streams.Length);
+            writer.Write(VertexCount);
+            writer.Write((uint)Streams.Count);
             writer.Write(IsStreaming);
 
-            foreach (VertexStream stream in Streams)
+            foreach (var stream in Streams)
             {
                 writer.Write(stream.Flags);
                 writer.Write(stream.Buffer.ElementStride);
-                writer.Write((uint)stream.UnknownData.Length);
+                writer.Write((uint)stream.ElementInfo.Count);
 
-                foreach (var bytes in stream.UnknownData)
-                    writer.Write(bytes);
+                foreach (var desc in stream.ElementInfo)
+                {
+                    writer.Write((byte)desc.ByteOffset);
+                    writer.Write((byte)desc.StorageType);
+                    writer.Write((byte)desc.ComponentCount);
+                    writer.Write((byte)desc.ElementType);
+                }
 
                 stream.GUID.ToData(writer);
                 stream.Buffer.ToData(writer);
