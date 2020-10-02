9142175045c0b65f3b1218ce888c38f35212fc7b
Nukem <Nukem@outlook.com>
2020-10-02 07:20:12 -0400

    Shove testing code into separate files for now

--- HZDCoreEditor/Program.cs
+++ HZDCoreEditor/Program.cs
@@ -3,221 +3,48 @@ using NAudio.Wave;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Windows.Forms;
 
 namespace HZDCoreEditor
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            //DecodeSavesTest();
-            //DecodeArchivesTest();
-            DecodeReserializeQuickTest();
-            //DecodeQuickTest();
-            //DecodeAllFilesTest();
-            //DecodeLocalizationTest();
-            //DecodeAudioTest();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            /*
+            Needs to be moved to its own unit test project
+
+            RTTI.SetGameMode(GameType.DS);
+            TestsDS.DecodeReserializeQuickCoreFilesTest();
+            TestsDS.DecodeReserializeAllCoreFilesTest();
+            TestsDS.DecodeQuickCoreFilesTest();
+            TestsDS.DecodeAllCoreFilesTest();
+            TestsDS.DecodeArchivesBenchmarkTest();
+            TestsDS.DecodeQuickArchivesTest();
+
+            RTTI.SetGameMode(GameType.HZD);
+            TestsHZD.DecodeSavesTest();
+            TestsHZD.DecodeReserializeQuickCoreFilesTest();
+            TestsHZD.DecodeReserializeAllCoreFilesTest();
+            TestsHZD.DecodeQuickCoreFilesTest();
+            TestsHZD.DecodeAllCoreFilesTest();
+            TestsHZD.DecodeQuickArchivesTest();
+            TestsHZD.DecodeAllArchivesTest();
+            */
+
+            /*
+            RTTI.SetGameMode(GameType.HZD);
+            ExtractHZDLocalization();
+            ExtractHZDAudio();
+            */
         }
 
-        static void DecodeSavesTest()
-        {
-            var a = new Decima.SaveGameProfile(@"C:\Users\Administrator\Documents\Horizon Zero Dawn\Saved Game\profile\profile.dat");
-            a.ReadProfile();
-
-            var b = new Decima.SaveGameSystem(@"C:\Users\Administrator\Documents\Horizon Zero Dawn\Saved Game\manualsave0\checkpoint.dat");
-
-            var c = new Decima.SaveGameSystem(@"C:\Users\Administrator\Documents\Horizon Zero Dawn\Saved Game\quicksave4\checkpoint.dat");
-        }
-
-        static void DecodeDeathStrandingArchivesTest()
-        {
-            string basePath = @"C:\Program Files (x86)\Steam\steamapps\common\Death Stranding\data";
-
-            var archiveList = new string[]
-            {
-                "7017f9bb9d52fc1c4433599203cc51b1.bin",
-            };
-
-            foreach (var file in archiveList)
-            {
-                var archive = new Packfile(Path.Combine(basePath, file));
-
-                foreach (var entry in archive.FileEntries)
-                {
-                    var physicalPath = Path.Combine(basePath, "extracted", $"{entry.PathHash}.bin");
-                    var physicalDir = Path.GetDirectoryName(physicalPath);
-
-                    Directory.CreateDirectory(physicalDir);
-                    archive.ExtractFile(entry.PathHash, physicalPath);
-                }
-            }
-        }
-
-        static void DecodeArchivesTest()
-        {
-            string basePath = @"C:\Program Files (x86)\Steam\steamapps\common\Horizon Zero Dawn\Packed_DX12\";
-
-            var archiveList = new string[]
-            {
-                "DLC1.bin",
-                "DLC1_English.bin",
-                "Initial.bin",
-                "Initial_English.bin",
-                "Remainder.bin",
-                "Remainder_English.bin",
-            };
-
-            foreach (var file in archiveList)
-            {
-                var indexFile = new PackfileIndex(Path.Combine(basePath, Path.ChangeExtension(file, ".idx")));
-                var archive = new Packfile(Path.Combine(basePath, file));
-
-                foreach (var entry in archive.FileEntries)
-                {
-                    if (!indexFile.ResolvePathByHash(entry.PathHash, out string fn))
-                        throw new Exception();
-
-                    var physicalPath = Path.Combine(basePath, "extracted", fn);
-                    var physicalDir = Path.GetDirectoryName(physicalPath);
-
-                    Directory.CreateDirectory(physicalDir);
-                    archive.ExtractFile(fn, physicalPath);
-                }
-
-                Console.WriteLine(file);
-            }
-        }
-
-        static void DecodeReserializeQuickTest()
-        {
-            var files = new string[]
-            {
-                @"models\weapons\anti_gravity_cannon\model\model.core",
-                @"entities\characters\models\humanoid_player.core",
-                @"models\characters\humans\heads\baby\babyaloy\animation\parts\head_lx.core",
-                @"models\characters\humans\hair\aloy\model\model.core",
-                @"models\animation_managers\characters\animals\blackrat\blackrat_blackrat.core",
-                @"sounds\music\world\world.core",
-                @"shaders\ecotope\texturesetarrays\terrain_texture_array.core",
-                @"animation_objects\ledge_climb\network\moaf_honst_fight_intro_network.core",
-                @"movies\movielist.core",
-                @"entities\trackedgamestats.core",
-                @"localized\sentences\aigenerated\nora_adult_male1_1\sentences.core",
-                @"interface\textures\markers\ui_marker_compass_bunker.core",
-                @"levels\worlds\world\tiles\tile_x05_y-01\layers\lighting\cubezones\cubemapzone_07_cube.core",
-                @"levels\worlds\world\tiles\tile_x05_y-01\layers\lighting\cubezones\cubezones_foundry_1.core",
-                @"textures\lighting_setups\skybox\star_field.core",
-                @"textures\base_maps\clouds_512.core",
-                @"textures\detail_textures\buildingblock\buildingblock_detailmap_array.core",
-                @"sounds\effects\dlc1\weapons\firebreather\wav\fire_loop_flames_01_m.core",
-                @"models\building_blocks\nora\dressing\components\dressing_b125_c006_resource.core",
-                @"telemetry\designtelemetry.core",
-                @"worlddata\worlddatapacking.core",
-                @"entities\weapons\heavy_weapons\heavy_railgun_cables.core",
-                @"entities\collectables\collectable_datapoint\datapoint_world.core",
-                @"entities\shops\shops.core",
-                @"levels\quests.core",
-                @"levels\game.core",
-                @"levels\worlds\world\leveldata\terrain.core",
-                @"weather\defaultweathersystem.core",
-                @"climates\regions\faro\faro_master_climate.core",
-                @"entities\armor\outfits\outfits.core",
-                @"models\weapons\raptordisc_playerversion\model\model.core",
-                @"loadouts\loadout.core",
-                @"levels\worlds\world\quests\mainquests\mq15_themountainthatfell_files\mq15_quest.core",
-                @"entities\characters\models\humanoid_civilian.core",
-                @"system\waves\white_noise_0dbfs.core",
-            };
-
-            foreach (string file in files)
-            {
-                string fullPath = Path.Combine(@"C:\Program Files (x86)\Steam\steamapps\common\Horizon Zero Dawn\Packed_DX12\extracted\", file);
-                Console.WriteLine(fullPath);
-
-                var objects = CoreBinary.Load(fullPath);
-
-                string tempPath = Path.ChangeExtension(fullPath, ".tmp");
-                CoreBinary.Save(tempPath, objects);
-
-                byte[] d1 = File.ReadAllBytes(fullPath);
-                byte[] d2 = File.ReadAllBytes(tempPath);
-
-                if (d1.Length != d2.Length)
-                    throw new Exception("Re-serialized file length doesn't match");
-
-                for (int i = 0; i < d1.Length; i++)
-                {
-                    if (d1[i] != d2[i])
-                        throw new Exception($"File data doesn't match at offset {i:X} in new file");
-                }
-
-                File.Delete(tempPath);
-            }
-        }
-
-        static void DecodeQuickTest()
-        {
-            var files = new string[]
-            {
-                @"models\weapons\anti_gravity_cannon\model\model.core",
-                @"entities\characters\models\humanoid_player.core",
-                @"models\characters\humans\heads\baby\babyaloy\animation\parts\head_lx.core",
-                @"models\characters\humans\hair\aloy\model\model.core",
-                @"models\animation_managers\characters\animals\blackrat\blackrat_blackrat.core",
-                @"sounds\music\world\world.core",
-                @"shaders\ecotope\texturesetarrays\terrain_texture_array.core",
-                @"animation_objects\ledge_climb\network\moaf_honst_fight_intro_network.core",
-                @"movies\movielist.core",
-                @"entities\trackedgamestats.core",
-                @"localized\sentences\aigenerated\nora_adult_male1_1\sentences.core",
-                @"interface\textures\markers\ui_marker_compass_bunker.core",
-                @"levels\worlds\world\tiles\tile_x05_y-01\layers\lighting\cubezones\cubemapzone_07_cube.core",
-                @"levels\worlds\world\tiles\tile_x05_y-01\layers\lighting\cubezones\cubezones_foundry_1.core",
-                @"textures\lighting_setups\skybox\star_field.core",
-                @"textures\base_maps\clouds_512.core",
-                @"textures\detail_textures\buildingblock\buildingblock_detailmap_array.core",
-                @"sounds\effects\dlc1\weapons\firebreather\wav\fire_loop_flames_01_m.core",
-                @"models\building_blocks\nora\dressing\components\dressing_b125_c006_resource.core",
-                @"telemetry\designtelemetry.core",
-                @"worlddata\worlddatapacking.core",
-                @"entities\weapons\heavy_weapons\heavy_railgun_cables.core",
-                @"entities\collectables\collectable_datapoint\datapoint_world.core",
-                @"entities\shops\shops.core",
-                @"levels\quests.core",
-                @"levels\game.core",
-                @"levels\worlds\world\leveldata\terrain.core",
-                @"weather\defaultweathersystem.core",
-                @"climates\regions\faro\faro_master_climate.core",
-                @"entities\armor\outfits\outfits.core",
-                @"models\weapons\raptordisc_playerversion\model\model.core",
-                @"loadouts\loadout.core",
-                @"levels\worlds\world\quests\mainquests\mq15_themountainthatfell_files\mq15_quest.core",
-                @"entities\characters\models\humanoid_civilian.core",
-                @"system\waves\white_noise_0dbfs.core",
-            };
-
-            foreach (string file in files)
-            {
-                string fullPath = Path.Combine(@"C:\Program Files (x86)\Steam\steamapps\common\Horizon Zero Dawn\Packed_DX12\extracted\", file);
-                Console.WriteLine(fullPath);
-
-                var objects = CoreBinary.Load(fullPath);
-            }
-        }
-
-        static void DecodeAllFilesTest()
-        {
-            var files = Directory.GetFiles(@"C:\Program Files (x86)\Steam\steamapps\common\Horizon Zero Dawn\Packed_DX12\extracted\", "*.core", SearchOption.AllDirectories);
-
-            foreach (string file in files)
-            {
-                Console.WriteLine(file);
-
-                var objects = CoreBinary.Load(file, true);
-            }
-        }
-
-        static void DecodeLocalizationTest()
+        static void ExtractHZDLocalization()
         {
             var files = Directory.GetFiles(@"C:\Program Files (x86)\Steam\steamapps\common\Horizon Zero Dawn\Packed_DX12\extracted\localized\", "*.core", SearchOption.AllDirectories);
             var allStrings = new List<string>();
@@ -241,7 +68,7 @@ namespace HZDCoreEditor
             File.WriteAllLines(@"C:\text_data_dump.txt", allStrings);
         }
 
-        static void DecodeAudioTest()
+        static void ExtractHZDAudio()
         {
             var files = Directory.GetFiles(@"C:\Program Files (x86)\Steam\steamapps\common\Horizon Zero Dawn\Packed_DX12\extracted\sounds\", "*.core", SearchOption.AllDirectories);
 
@@ -249,7 +76,7 @@ namespace HZDCoreEditor
             {
                 Console.WriteLine(file);
 
-                var coreObjects = CoreBinary.Load(file, true);
+                var coreObjects = CoreBinary.Load(file);
 
                 foreach (var obj in coreObjects)
                 {
@@ -268,7 +95,7 @@ namespace HZDCoreEditor
                         if (wave.Encoding == Decima.HZD.EWaveDataEncoding.MP3)
                             rs = new RawSourceWaveStream(ms, new Mp3WaveFormat(wave.SampleRate, wave.ChannelCount, wave.FrameSize, (int)wave.BitsPerSecond));
                         else if (wave.Encoding == Decima.HZD.EWaveDataEncoding.PCM)
-                            rs = new RawSourceWaveStream(ms, new WaveFormat(wave.SampleRate, 16, wave.ChannelCount));
+                            rs = new RawSourceWaveStream(ms, new WaveFormat(wave.SampleRate, 16, wave.ChannelCount));// This is wrong
 
                         string outFile = Path.Combine(Path.GetDirectoryName(file), wave.Name.Value + ".wav");
 
---
+++ HZDCoreEditor/TestsDS.cs
@@ -0,0 +1,163 @@
+﻿using Decima;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace HZDCoreEditor
+{
+    public static class TestsDS
+    {
+        private static readonly string GameDataPath = @"C:\Program Files (x86)\Steam\steamapps\common\Death Stranding\data";
+        private static readonly string GameDataPathExtracted = @"C:\Program Files (x86)\Steam\steamapps\common\Death Stranding\data\extracted";
+
+        private static readonly string[] QuickTestArchives = new string[]
+        {
+            "7017f9bb9d52fc1c4433599203cc51b1.bin",
+        };
+
+        private static readonly string[] QuickTestFiles = new string[]
+        {
+            @"interface\menu\ds\ui_menu_hold_legend_prefab.core",
+            @"ds\radio\radio_sequence\global\rd_menu_special_new_item.core",
+            @"ds\location\area01\pre_constructions\area01_pre_constructions.core",
+            @"ds\sounds\wwise_bnk_collections\sd_wwise_bnk_collection_game_resident.core",
+            @"ds\textures\detail_textures\dsdamagetexturearray.core",
+            @"localized\sentences\ds\loc_object_names.core",
+            @"ds\generic_demo\sqn_common_sq_cs99_s00500.core",
+            @"ds\generic_demo\sqn_common_sq_cs99_s00400.core",
+            @"game\ds_online_sync_config.core",
+            @"ambience\newambiencemanager.core",
+            @"ds\models\characters\sam_sam\core\sam_body_black\model\parts\mesh_bodynaked_lx.core",
+            @"ds\models\characters\sam_sam\core\sam_body_black\model\parts\mesh_upperbody_lx.core",
+        };
+
+        public static void DecodeReserializeQuickCoreFilesTest()
+        {
+            foreach (string file in QuickTestFiles)
+            {
+                string fullPath = Path.Combine(GameDataPathExtracted, file);
+                Console.WriteLine(fullPath);
+
+                var objects = CoreBinary.Load(fullPath);
+
+                string tempPath = Path.ChangeExtension(fullPath, ".tmp");
+                CoreBinary.Save(tempPath, objects);
+
+                byte[] d1 = File.ReadAllBytes(fullPath);
+                byte[] d2 = File.ReadAllBytes(tempPath);
+
+                if (d1.Length != d2.Length)
+                    throw new Exception("Re-serialized file length doesn't match");
+
+                for (int i = 0; i < d1.Length; i++)
+                {
+                    if (d1[i] != d2[i])
+                        throw new Exception($"File data doesn't match at offset {i:X} in new file");
+                }
+
+                File.Delete(tempPath);
+            }
+        }
+
+        public static void DecodeReserializeAllCoreFilesTest()
+        {
+            var files = Directory.GetFiles(GameDataPathExtracted, "*.core", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+
+                var objects = CoreBinary.Load(file, true);
+
+                string tempPath = Path.ChangeExtension(file, ".tmp");
+                CoreBinary.Save(tempPath, objects);
+
+                byte[] d1 = File.ReadAllBytes(file);
+                byte[] d2 = File.ReadAllBytes(tempPath);
+
+                if (d1.Length != d2.Length)
+                    throw new Exception("Re-serialized file length doesn't match");
+
+                for (int i = 0; i < d1.Length; i++)
+                {
+                    if (d1[i] != d2[i])
+                        throw new Exception($"File data doesn't match at offset {i:X} in new file");
+                }
+
+                File.Delete(tempPath);
+            }
+        }
+
+        public static void DecodeQuickCoreFilesTest()
+        {
+            foreach (string file in QuickTestFiles)
+            {
+                string fullPath = Path.Combine(GameDataPathExtracted, file);
+                Console.WriteLine(fullPath);
+
+                var objects = CoreBinary.Load(fullPath);
+            }
+        }
+
+        public static void DecodeAllCoreFilesTest()
+        {
+            var files = Directory.GetFiles(GameDataPathExtracted, "*.core", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+
+                var objects = CoreBinary.Load(file, true);
+            }
+        }
+
+        public static void DecodeArchivesBenchmarkTest()
+        {
+            string basePath = GameDataPath;
+
+            string prefetchPath = Path.Combine(basePath, "extracted", "prefetch/fullgame.prefetch.core");
+            Directory.CreateDirectory(Path.GetDirectoryName(prefetchPath));
+
+            var archive = new Packfile(Path.Combine(basePath, "7017f9bb9d52fc1c4433599203cc51b1.bin"));
+            var sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < 500; i++)
+                archive.ExtractFile("prefetch/fullgame.prefetch.core", prefetchPath, true);
+
+            sw.Stop();
+            Debugger.Log(0, "Info", $"Repeated archive extraction took {sw.ElapsedMilliseconds} milliseconds");
+        }
+
+        public static void DecodeQuickArchivesTest()
+        {
+            string basePath = GameDataPath;
+
+            foreach (var file in QuickTestArchives)
+            {
+                var archive = new Packfile(Path.Combine(basePath, file));
+
+                // Prefetch has to be extracted first in order to get any kind of file names
+                string prefetchPath = Path.Combine(basePath, "extracted", "prefetch/fullgame.prefetch.core");
+                Directory.CreateDirectory(Path.GetDirectoryName(prefetchPath));
+
+                archive.ExtractFile("prefetch/fullgame.prefetch.core", prefetchPath, true);
+                var prefetchObject = CoreBinary.Load(prefetchPath)[0] as Decima.DS.PrefetchList;
+
+                // Do the /actual/ extraction
+                foreach (var assetPath in prefetchObject.Files)
+                {
+                    string localCorePath = assetPath.Path + ".core";
+
+                    if (!archive.FileExists(localCorePath))
+                        continue;
+
+                    var physicalPath = Path.Combine(basePath, "extracted", localCorePath);
+                    var physicalDir = Path.GetDirectoryName(physicalPath);
+
+                    Directory.CreateDirectory(physicalDir);
+                    archive.ExtractFile(localCorePath, physicalPath, true);
+                }
+            }
+        }
+    }
+}
