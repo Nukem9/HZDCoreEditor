c5e95a1ae178a317df7548c759e0115d76fec5c5
Nukem <Nukem@outlook.com>
2021-10-15 20:49:08 -0400

    Merge local branch 'origin/master' into akinikites
    restore deleted files from previous commits

---
+++ hzd_test/common.cpp
@@ -0,0 +1,15 @@
+#include "common.h"
+
+uintptr_t g_ModuleBase;
+uintptr_t g_ModuleSize;
+
+uintptr_t g_CodeBase;	// .text or .textbss
+uintptr_t g_CodeEnd;
+uintptr_t g_RdataBase;	// .rdata
+uintptr_t g_RdataEnd;
+uintptr_t g_DataBase;	// .data
+uintptr_t g_DataEnd;
+
+GameType g_GameType;
+char g_GamePrefix[64];
+std::unordered_map<std::string, uintptr_t> g_OffsetMap;
---
+++ hzd_test/common.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <stddef.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+
+#include <detours/Detours.h>
+#pragma comment(lib, "detours.lib")
+
+#include "xutil.h"
+
+extern uintptr_t g_ModuleBase;
+extern uintptr_t g_ModuleSize;
+
+extern uintptr_t g_CodeBase;	// .text or .textbss
+extern uintptr_t g_CodeEnd;
+extern uintptr_t g_RdataBase;	// .rdata
+extern uintptr_t g_RdataEnd;
+extern uintptr_t g_DataBase;	// .data
+extern uintptr_t g_DataEnd;
+
+enum class GameType
+{
+	DeathStranding = 0,
+	HorizonZeroDawn = 1,
+};
+
+extern GameType g_GameType;
+extern char g_GamePrefix[64];
+extern std::unordered_map<std::string, uintptr_t> g_OffsetMap;
