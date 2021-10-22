e8ac85b7ef204a4cea27f712b2857b7b82169e39
Nukem <Nukem@outlook.com>
2021-10-21 21:11:26 -0400

    Add (currently unused) tests for save game profiles

---
+++ HZDCoreEditorTests/SaveGameTests.cs
@@ -0,0 +1,35 @@
+﻿using Decima;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HZDCoreEditorTests
+{
+    [TestClass]
+    public class SaveGameTests
+    {
+        [Ignore]
+        [TestCategory("GameBasic")]
+        [TestMethod]
+        public void TestReadManualSaveFile()
+        {
+            var s = new SaveGameSystem(@"C:\Users\Administrator\Documents\Horizon Zero Dawn\Saved Game\manualsave0\checkpoint.dat");
+        }
+
+        [Ignore]
+        [TestCategory("GameBasic")]
+        [TestMethod]
+        public void TestReadQuickSaveFile()
+        {
+            var s = new SaveGameSystem(@"C:\Users\Administrator\Documents\Horizon Zero Dawn\Saved Game\quicksave4\checkpoint.dat");
+        }
+
+        [Ignore]
+        [TestCategory("GameBasic")]
+        [TestMethod]
+        public void TestReadSaveProfile()
+        {
+            var s = new SaveGameProfile(@"C:\Users\Administrator\Documents\Horizon Zero Dawn\Saved Game\profile\profile.dat");
+
+            s.ReadProfile();
+        }
+    }
+}
