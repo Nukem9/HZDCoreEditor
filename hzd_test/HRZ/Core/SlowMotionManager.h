fa3b7a3060d21e205a8a117ce670d2f15e73ad17
Nukem <Nukem@outlook.com>
2021-12-22 20:52:28 -0500

    DLL: Drop SlowMotionManager functions

--- hzd_test/HRZ/Core/SlowMotionManager.h
+++ hzd_test/HRZ/Core/SlowMotionManager.h
@@ -24,16 +24,6 @@ public:
 	Array<TimescaleEntry> m_TimescaleModifiers;
 	float m_CurrentTimescale;
 	float m_CurrentCameraTimescale;
-
-	TimescaleModifierId AddTimescaleModifier(float Timescale, float CameraTimescale, float BlendInTime)
-	{
-		return Offsets::CallID<"SlowMotionManager::AddTimescaleModifier", TimescaleModifierId(*)(SlowMotionManager *, float, float, float)>(this, Timescale, CameraTimescale, BlendInTime);
-	}
-
-	void RemoveTimescaleModifier(TimescaleModifierId& Id, float BlendOutTime)
-	{
-		return Offsets::CallID<"SlowMotionManager::RemoveTimescaleModifier", void(*)(SlowMotionManager *, TimescaleModifierId&, float)>(this, Id, BlendOutTime);
-	}
 };
 assert_offset(SlowMotionManager, m_TimescaleModifiers, 0x8);
 assert_offset(SlowMotionManager, m_CurrentTimescale, 0x18);
