54b9f3f093ba784278e8ac735613d068aff83db4
Nukem <Nukem@outlook.com>
2022-01-03 18:40:44 -0500

    DLL: Manually implement WorldState::SetTimeOfDay

--- hzd_test/HRZ/Core/WorldState.h
+++ hzd_test/HRZ/Core/WorldState.h
@@ -25,7 +25,7 @@ public:
 	uint32_t m_DayNightCycleCount;				// 0xA0
 	bool m_DayNightCycleEnabled;				// 0xA4
 	bool m_PauseTimeOfDay;						// 0xA5
-	float m_FastForwardDuration;				// 0xA8
+	float m_FastForwardAmount;					// 0xA8
 	float m_FastForwardAmountPerTick;			// 0xAC
 	Ref<WorldStateResource> m_Resource;			// 0xB0
 
@@ -34,7 +34,32 @@ public:
 
 	void SetTimeOfDay(float Time, float FastForwardDuration)
 	{
-		Offsets::CallID<"WorldState::SetTimeOfDay", void(*)(WorldState *, float, float)>(this, Time, FastForwardDuration);
+		const float currentTime = m_TimeOfDay;
+
+		if (FastForwardDuration <= 0)
+		{
+			// The original game code is incredibly stupid and complex for what amounts to this (simplified) equation
+			float newTime = Time;
+
+			if (Time < currentTime)
+				newTime = Time + 24;
+
+			while (newTime >= 24)
+			{
+				newTime -= 24;
+				m_DayNightCycleCount++;
+			}
+
+			m_TimeOfDay = newTime;
+			m_FastForwardAmount = 0;
+		}
+		else
+		{
+			float fastForwardHours = (Time < currentTime) ? (24 - currentTime + Time) : (Time - currentTime);
+
+			m_FastForwardAmount = fastForwardHours * 3600;
+			m_FastForwardAmountPerTick = m_FastForwardAmount / FastForwardDuration;
+		}
 	}
 };
 assert_offset(WorldState, m_PropertyContainer, 0x20);
